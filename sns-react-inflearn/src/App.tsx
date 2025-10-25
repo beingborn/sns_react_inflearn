9671a092b306b70f6a17aa977191a06a673658ba
beingborn <dolmin0708@naver.com>
2025-10-25 17:35:38 +0900

    first commit

---
+++ sns-react-inflearn/src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <div>
+      <Button>버튼 1</Button>
+      <div className="text-primary">Primary</div>
+    </div>
+  );
+}
+
+export default App;
+
+// Tailwind를 위한 확장 프로그램
+// Tailwind CSS IntelliSense
