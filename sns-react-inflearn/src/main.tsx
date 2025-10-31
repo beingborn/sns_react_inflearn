debf177f2e0033f2766f43d3875fc08918ed1943
beingborn <dolmin0708@naver.com>
2025-10-31 13:19:52 +0900

    Tan Stack Install, Todos Data Fetching

--- sns-react-inflearn/src/main.tsx
+++ sns-react-inflearn/src/main.tsx
@@ -1,10 +1,19 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
 import App from "./App.tsx";
 import "./index.css";
 
+const queryClient = new QueryClient();
+
+/*
+    Query Client => 모든 서버 상태를 보관하는 일종의 저장소 : Store
+*/
+
 createRoot(document.getElementById("root")!).render(
     <BrowserRouter>
-        <App />
+        <QueryClientProvider client={queryClient}>
+            <App />
+        </QueryClientProvider>
     </BrowserRouter>,
 );
